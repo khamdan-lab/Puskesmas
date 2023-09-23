130aa6e4d178f5d370d2708bf073fd1802ebd70a
Khamdan Farizqi <khamdanfarizqi@gmail.com>
2023-09-23 21:36:02 +0700

    change database password in file koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
  <!-- koneksi terhadap database -->
 
-<?php $koneksi = mysqli_connect('localhost','khamdan','Kh@mdan1','puskesmas');
+<?php $koneksi = mysqli_connect('localhost','khamdan','Kh@mdan123','puskesmas');
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
